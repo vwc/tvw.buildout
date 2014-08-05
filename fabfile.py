8311f74daf64d00b8d01936350e591897baf7498
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-08-05 21:02:35 +0200

    Backport current fabfile setup

--- fabfile.py
+++ fabfile.py
@@ -1,54 +1,32 @@
+from fabric.api import task
 from fabric.api import cd
 from fabric.api import env
-from fabric.api import local
 from fabric.api import run
-from fabric.api import sudo
-from fabric.api import task
 
-from ade25.fabfiles import server
 from ade25.fabfiles import project
+
 
 env.use_ssh_config = True
 env.forward_agent = True
 env.port = '22222'
 env.user = 'root'
-env.hosts = ['6zu4']
+env.hosts = ['z13']
 env.webserver = '/opt/webserver/buildout.webserver'
-env.code_root = '/opt/sites/plonesite/buildout.plonesite'
-env.local_root = '/Users/anjaradtke/daprogramm/tvw/buildout.tvw'
-env.sitename = 'plonesite'
+env.code_root = '/opt/sites/agita/buildout.agita'
+env.local_root = '/Users/sd/dev/agita/buildout.agita'
+env.sitename = 'agita'
 env.code_user = 'root'
 env.prod_user = 'www'
 
 
 @task
-def bo_conf():
-    #t = cuisine.file_local_read('packages.cfg')
-    filename = '%s/packages.cfg' % env.local_root
-    with open(filename, 'r+a') as f:
-        read_data = f.read()
-        # config = ConfigParser.ConfigParser()
-        print read_data
-
-
-def ls():
-    """ Low level configuration test """
-    with cd(env.code_root):
-        run('ls')
-
-
-def supervisorctl(*cmd):
-    """Runs an arbitrary supervisorctl command."""
-    with cd(env.webserver):
-        run('bin/supervisorctl ' + ' '.join(cmd))
-
-
 def deploy():
     """ Deploy current master to production server """
     project.site.update()
-    project.site.estart()
+    project.site.restart()
 
 
+@task
 def deploy_full():
     """ Deploy current master to production and run buildout """
     project.site.update()
@@ -56,8 +34,22 @@ def deploy_full():
     project.site.restart()
 
 
+@task
 def rebuild():
     """ Deploy current master and run full buildout """
     project.site.update()
     project.site.build_full()
     project.site.restart()
+
+
+@task
+def develop(*cmd):
+    """Runs arbitrary mr.developer bin/develop command."""
+    with cd(env.code_root):
+        run('nice bin/develop ' + ' '.join(cmd))
+
+
+@task
+def get_data():
+    """ Copy live database for local development """
+    project.db.download_data()
