d17283d88e6307380be1bb2c867fe7d4e3a42300
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-08-05 20:56:19 +0200

    Rename package
    In order to nake some sense of the source code when looking at it, we
    use a better naming than the default

--- src/tvw.sitetheme/setup.py
+++ src/tvw.sitetheme/setup.py
@@ -3,9 +3,9 @@ import os
 
 version = '1.0'
 
-setup(name='example.sitetheme',
+setup(name='tvw.sitetheme',
       version=version,
-      description="Example Diazo Plone Theme",
+      description="tvw Diazo Plone Theme",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
@@ -20,7 +20,7 @@ setup(name='example.sitetheme',
       url='http://dist.vorwaerts-werbung.de',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['example'],
+      namespace_packages=['tvw'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
--- src/tvw.sitetheme/tvw/sitetheme/resources/overrides/README.txt
+++ src/tvw.sitetheme/tvw/sitetheme/resources/overrides/README.txt
@@ -4,9 +4,9 @@ You should preferable use this directory to override Plone and Plone Addon
 templates, by copying the original template and renaming it to the full
 dotted name.
 
-Usage example:
+Usage tvw:
 
-For example, to override logo.pt in plone.app.layout.viewlets, which is found
+For tvw, to override logo.pt in plone.app.layout.viewlets, which is found
 in plone/app/layout/viewlets/logo.pt inside the plone.app.layout distribution,
 you would copy logo.pt into the overrides directory as
 
