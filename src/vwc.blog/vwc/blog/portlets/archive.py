de3b950bde431e53ca20735592f46bdc8ddac4f9
pchanxxx <p.chanxxx@gmail.com>
2014-08-28 14:13:43 +0200

    content changes, style fixes and blog archive changes

--- src/vwc.blog/vwc/blog/portlets/archive.py
+++ src/vwc.blog/vwc/blog/portlets/archive.py
@@ -28,29 +28,24 @@ MONTHVOCAB = {
     'month_12': _(u'month_12'),
 }
 
-
 class IArchivePortlet(IPortletDataProvider):
     """A portlet
-
     It inherits from IPortletDataProvider because for this portlet, the
     data that is being rendered and the portlet assignment itself are the
     same.
     """
     archive_view = schema.TextLine(
-        title=_(u"Archive view"),
-        description=_(u"The name of the archive view"),
-        default=u'blog-view',
-        required=True
+    title=_(u"Archive view"),
+    description=_(u"The name of the archive view"),
+    default=u'blog-view',
+    required=True
     )
-
 
 class Assignment(base.Assignment):
     """Portlet assignment.
-
     This is what is actually managed through the portlets UI and associated
     with columns.
     """
-
     implements(IArchivePortlet)
 
     archive_view = u'blog_view'
@@ -64,7 +59,6 @@ class Assignment(base.Assignment):
         "manage portlets" screen.
         """
         return _("Blog Monthly archive")
-
 
 class Renderer(base.Renderer):
     """Portlet renderer.
@@ -81,13 +75,15 @@ class Renderer(base.Renderer):
         catalog = getToolByName(self.context, 'portal_catalog')
         # Get the path of where the portlet is created. That's the blog.
         assignment_context = find_portlet_assignment_context(self.data,
-                                                             self.context)
+        self.context)
+
         if assignment_context is None:
             assignment_context = self.context
-        self.folder_path = '/'.join(assignment_context.getPhysicalPath())
-        self.folder_url = assignment_context.absolute_url()
-        brains = catalog(path={'query': self.folder_path, 'depth': 3},
-                         object_provides=IBlogEntry.__identifier__,)
+            self.folder_path = '/'.join(assignment_context.getPhysicalPath())
+            self.folder_url = assignment_context.absolute_url()
+            brains = catalog(path={'query': self.folder_path, 'depth': 3},
+            object_provides=IBlogEntry.__identifier__,)
+
         if not brains:
             return
         # Count the number of posts per month:
@@ -96,7 +92,7 @@ class Renderer(base.Renderer):
             effective = brain.effective
             year = str(effective.year())
             if year == '1000':
-                continue  # No effective date == not published
+                continue # No effective date == not published
             month = str(effective.month())
             count = allmonths.setdefault((year, month), 0)
             allmonths[(year, month)] = count + 1
@@ -111,12 +107,12 @@ class Renderer(base.Renderer):
             months[month] = allmonths[year, month]
 
     def years(self):
-        return sorted(self._counts.keys())
+        return sorted(self._counts.keys(), reverse=True)
 
     def months(self, year):
         # sort as integers, return as strings
         _months = sorted([int(m) for m in self._counts[year].keys()],
-                         reverse=True)
+        reverse=True)
         return [str(m) for m in _months]
 
     def count(self, year, month):
@@ -124,13 +120,11 @@ class Renderer(base.Renderer):
 
     def archive_url(self, year, month):
         return '%s/%s?year=%s&month=%s' % (self.folder_url,
-                                           self.data.archive_view,
-                                           year, month)
-
+        self.data.archive_view,
+        year, month)
 
 class AddForm(base.AddForm):
     """Portlet add form.
-
     This is registered in configure.zcml. The form_fields variable tells
     zope.formlib which fields to display. The create() method actually
     constructs the assignment that is being added.
@@ -140,10 +134,8 @@ class AddForm(base.AddForm):
     def create(self, data):
         return Assignment(**data)
 
-
 class EditForm(base.EditForm):
     """Portlet edit form.
-
     This is registered with configure.zcml. The form_fields variable tells
     zope.formlib which fields to display.
     """
